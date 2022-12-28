d71046916e6cd6356024fd553eec9775ee37fec0
RobertoAC2016 <keepteacher@gmail.com>
2022-12-28 11:28:36 -0600

    Update env.py

--- env.py
+++ env.py
@@ -1,5 +1,3 @@
-api_id = "27401208"                                         #'API_id'
-api_hash = "6cb9d8b822e2af9089ce006d5b8dfd71"               #'API_hash'
-# token = "5643028327:AAEkqFkfiUKjuwTEHB8n7H57SYnZZN1zGLI"    #'bot token'
+
 token = "5660966047:AAHrvg6LnLaL89ZjPzp17geQQqOleWs01wc"
 path = "D:\\RAC\\NET\\pythonTelegramMsg\\"
